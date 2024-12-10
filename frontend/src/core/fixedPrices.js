d1bb01edbec0310a981cb4a474c470b5dc49d467
ThaGreatFikshun <jaycomputertechnologies@gmail.com>
2024-12-10 11:15:00 +0300

    Renamed client to frontend.

---
+++ frontend/src/core/fixedPrices.js
@@ -0,0 +1,32 @@
+export const prices = [
+  {
+    _id: 0,
+    name: 'Any',
+    array: [],
+  },
+  {
+    _id: 1,
+    name: 'Ksh 0 to Ksh 999',
+    array: [0, 999],
+  },
+  {
+    _id: 2,
+    name: 'Ksh 1,000 to Ksh 4,999',
+    array: [1000, 4999],
+  },
+  {
+    _id: 3,
+    name: 'Ksh 5,000 to Ksh 9,999',
+    array: [5000, 9999],
+  },
+  {
+    _id: 4,
+    name: 'Ksh 10,000 to Ksh 19,999',
+    array: [10000, 19999],
+  },
+  {
+    _id: 5,
+    name: 'More than Ksh 20,000',
+    array: [20000, 999999],
+  },
+];
